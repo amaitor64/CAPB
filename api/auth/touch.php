1613a0b6ba92e1ddb22ab06c582799ebd2f3de04
amaitor64 <jerome.turnaco@icloud.com>
2026-05-06 22:27:45 +0200

    Preserve id_token when refreshing session

--- api/auth/touch.php
+++ api/auth/touch.php
@@ -10,5 +10,6 @@ if (!$session) {
 }
 
 $secret = capb_auth_secret();
-capb_set_session_cookie(capb_create_session_token((string) $session['email'], $secret));
+$idToken = is_string($session['id_token'] ?? null) ? (string) $session['id_token'] : null;
+capb_set_session_cookie(capb_create_session_token((string) $session['email'], $secret, $idToken));
 capb_json(['ok' => true], 200, ['Cache-Control' => 'no-store']);
